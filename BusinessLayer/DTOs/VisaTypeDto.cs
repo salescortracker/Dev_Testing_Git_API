fefcf2313f31810da068bcb44d31a27bad01fa27
Kittu-Mobiles <krishna@cortracker360.com>
2026-03-20 14:05:38 +0530

    Visatype-API-Kittu

--- BusinessLayer/DTOs/VisaTypeDto.cs
+++ BusinessLayer/DTOs/VisaTypeDto.cs
@@ -9,8 +9,32 @@ namespace BusinessLayer.DTOs
     public class VisaTypeDto
     {
         public int VisaTypeId { get; set; }
+
         public int CompanyId { get; set; }
+
         public int RegionId { get; set; }
-        public string VisaTypeName { get; set; } = null!;
+
+        public string VisaType1 { get; set; } = null!;
+        public string VisaTypeName
+        {
+            get => VisaType1;
+            set => VisaType1 = value;
+        }
+
+        public string? Description { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public bool? IsDeleted { get; set; }
+
+        public int? CreatedBy { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public int? ModifiedBy { get; set; }
+
+        public DateTime? ModifiedAt { get; set; }
+        public string RegionName { get; set; }
+        public string CompanyName { get; set; }
     }
 }
